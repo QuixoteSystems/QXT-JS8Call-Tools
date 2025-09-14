51bbbedcb5b33767547da7395bdc7c956853fd4a
Quixote <82296005+QuixoteSystems@users.noreply.github.com>
2025-09-14 13:01:16 +0200

    Update config.py

--- Telegram-Bridge/config.py
+++ Telegram-Bridge/config.py
@@ -7,23 +7,23 @@ Archivo de configuracion donde poner tus parametros y datos
 
 # ===================== CONFIG =====================
 
-TELEGRAM_BOT_TOKEN = "111111111111111111111111111111111"     # Puedes obtenerlo el token de tu bot (desde @BotFather).
-TELEGRAM_CHAT_ID   = 1065228100                              # Reemplaza por tu chat ID (int) el chat ID donde quieres recibir/enviar (p.ej., tu chat privado; puedes obtenerlo hablando >
-MY_CALLSIGN        = ["EA1ABC"]                                # Tu indicativo en JS8
-GRID               = "IMHO"                                  # Tu localizacion del Maidenhead Grid
+TELEGRAM_BOT_TOKEN = "111111111111111111111111111111111"     # You can get using @BotFather in Telegram.
+TELEGRAM_CHAT_ID   = 1065228100                              # Change for your chat ID (int) that is your user chat ID on TG where you want receive/send messages (i.e. private chat, group, channel...)
+MY_CALLSIGN        = ["EA1ABC"]                              # Your Callsign in JS8Call
+GRID               = "IMHO"                                  # Your Maidenhead Grid location
 MY_ALIASES         = [MY_CALLSIGN, "EA2ABC"]                 # Tu indicativo en JS8, si usas mas de uno
-MONITORED_GROUPS   = ["@QXTNET"]                             # Ejemplo de grupos JS8 que quieres escuchar
-JS8_HOST           = "127.0.0.1"                             # IP de la maquina donde esta corriendo JS8Call
-JS8_PORT           = 2442                                    # Puerto JS8Call API JSON (normalmente 2442)
-TRANSPORT          = "TCP"                                   # Protocolo del Puerto "TCP" (recomendado) o "UDP"
+MONITORED_GROUPS   = ["@HB","@QXTNET"]                       # Example to receive all messages of Hearbeat and QXTNET Group
+JS8_HOST           = "127.0.0.1"                             # IP where is running JS8Call, leave 127.0.0.1 if you are running this script in the same machine
+JS8_PORT           = 2442                                    # Port JS8Call API JSON (normalmente 2442)
+TRANSPORT          = "TCP"                                   # Protocol "TCP" (recommended) or "UDP"
 LANG               = "es"                                    # Write "en" for English strings
 
 
-LEVEL                      = "INFO"                                  # Logging level, it can be: WARNING, CRITICAL, INFO, DEBUG...
-IGNORE_MESSAGES_FROM_SELF  = True           # Para seguridad: evita bucles reenviando lo que tú mismo transmites
-FORWARD_QSO_WINDOW         = True                  # activa el sondeo del QSO window
-QSO_POLL_SECONDS           = 2.0                   # intervalo de sondeo
-QSO_ID_CACHE_SIZE          = 2000                   # cuántos IDs recordamos para no duplicar
+LEVEL                      = "INFO"                          # Logging level, it can be: WARNING, CRITICAL, INFO, DEBUG...
+IGNORE_MESSAGES_FROM_SELF  = True                            # Para seguridad: evita bucles reenviando lo que tú mismo transmites
+FORWARD_QSO_WINDOW         = True                            # activa el sondeo del QSO window
+QSO_POLL_SECONDS           = 2.0                             # intervalo de sondeo
+QSO_ID_CACHE_SIZE          = 2000                            # cuántos IDs recordamos para no duplicar
 
 # ======= TELEGRAM
 
